08195daaed3518e67327c1288c8fd6bbcf88044b
mrprez <mrprez@mrprez-PC>
2021-05-14 18:24:13 +0200

    Correction des TU

--- src/test/java/com/mrprez/gencross/online/service/UserServiceTest.java
+++ src/test/java/com/mrprez/gencross/online/service/UserServiceTest.java
@@ -4,19 +4,22 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.mrprez.gencross.online.dao.UserDao;
 import com.mrprez.gencross.online.model.User;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 public class UserServiceTest {
 	
 	@InjectMocks
